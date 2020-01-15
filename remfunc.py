9a0f203a127b6182f6e53f0ca84222fe29d86735
shakilattar <59828272+shakilattar@users.noreply.github.com>
2020-01-15 19:34:30 +0530

    Create remfunc.py

---
+++ remfunc.py
@@ -0,0 +1,9 @@
+def remfunc():
+    numbers = [144, 169, 196, 121, 225, 289, 121]
+    print(numbers)
+    # remove the first occurrence of value 121
+    numbers.remove(121)
+    print(numbers)
+
+
+remfunc()
